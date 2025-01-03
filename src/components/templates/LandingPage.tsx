07af3db974451808258631770259addebe5ffff7
ADS-CORP <alex@adscorp.net>
2025-01-03 01:41:46 -0400

    “pageSpacing”

--- src/components/templates/LandingPage.tsx
+++ src/components/templates/LandingPage.tsx
@@ -157,10 +157,10 @@ export function LandingPage({ brand, content, source, quizId, buyer }: LandingPa
         >
           <TrafficCounter brand={brand} />
         </div>
-        <div className="relative mt-[120px]">
-          <div className="max-w-screen-xl mx-auto px-4 md:px-6 lg:px-8 py-12">
+        <div className="relative mt-[90px]">
+          <div className="max-w-screen-xl mx-auto px-4 md:px-6 lg:px-8 py-5">
             <div className="text-center">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
                 {content.headline}
               </h1>
             </div>
@@ -168,7 +168,7 @@ export function LandingPage({ brand, content, source, quizId, buyer }: LandingPa
 
           <div className="relative bg-white">
             <div className="max-w-screen-xl mx-auto px-4 md:px-6 lg:px-8">
-              <div className="relative py-8">
+              <div className="relative py-3">
                 <div id="quiz-widget-container" className="relative">
                   <QuizWidget quizConfig={content.quizConfig} quizId={quizId} brand={brand} />
                 </div>
