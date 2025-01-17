d4421a176c2c9ea5143f4114785baec1912ec1c1
ADS-CORP <alex@adscorp.net>
2025-01-17 16:24:25 -0400

    new-pages

--- src/app/(pages)/pj/oxb-typh-fb/page.tsx
+++ src/app/(pages)/pj/oxb-typh-fb/page.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 const Page = () => {
   const cta = undefined;
   const ctaText = undefined;
-  const showEmail = undefined;
+  const showEmail = true;
   const showCta = undefined;
   
   // Format CTA URL if needed
